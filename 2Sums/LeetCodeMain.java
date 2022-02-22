de6f06645cad9e4eb242b2fc8d7dbc252bd5fec3
Yao Wang <dell9459@gmail.com>
2022-02-22 00:25:24 -0500

    finished 2Sums

---
+++ 2Sums/LeetCodeMain.java
@@ -0,0 +1,25 @@
+package LeetCodes;
+
+import java.util.Scanner;
+public class LeetCodeMain {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Size?");
+
+        int size = scanner.nextInt();
+        int []example = new int[size];
+        for (int i =0;i<example.length;i++){
+            int value = scanner.nextInt();
+            example[i] = value;
+        }
+        System.out.println("Target?");
+        int target = scanner.nextInt();
+        int []temp =solution.twoSum(example,target);
+        for (int i = 0; i < temp.length; i++) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+}
