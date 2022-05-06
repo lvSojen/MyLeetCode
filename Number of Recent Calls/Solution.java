e433df436410cdc1873586a0df91c751630a8e08
Yao Wang <ywang011@odu.edu>
2022-05-06 12:59:30 -0400

    finished solution

---
+++ Number of Recent Calls/Solution.java
@@ -0,0 +1,20 @@
+class RecentCounter {
+    ArrayList<Integer> req;
+    public RecentCounter() {
+        req= new ArrayList<>();
+    }
+    
+    public int ping(int t) {
+        req.add(t);
+        while(req.get(0)<t-3000){
+            req.remove(0);
+        }
+        return req.size();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
