79f36ba32d5b25a750fabb03f2a7b55ca31706de
Yao Wang <ywang011@odu.edu>
2022-06-10 17:24:23 -0400

    edited 2.

--- 2. Sums/Solution.java
+++ 2. Sums/Solution.java
@@ -1,5 +1,5 @@
-package LeetCodes;
-
+// Link
+// https://leetcode.com/submissions/detail/719164693/
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int [] answer= new int[2];
@@ -13,6 +13,25 @@ class Solution {
         }
         return answer;
     }
-
-
+}
+// Alternative
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int count =0;
+        for(int number : nums) {
+            map.put(number,count++);
+        }
+        int[] res = new int[2];
+        count =0;
+        for(int number : nums) {
+            if(map.containsKey(target-number) && map.get(target-number)!=count) {
+                res[0] = count;
+                res[1] = map.get(target-number);
+                break;
+            }
+            count++;
+        }
+        return res;
+    }
 }
